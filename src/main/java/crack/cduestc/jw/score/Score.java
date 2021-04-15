0e5e39fbda999d3c269154f449279db752da5a5f
19982004416 <404213506@qq.com>
2021-04-15 21:32:32 +0800

    初始化仓库

---
+++ src/main/java/crack/cduestc/jw/score/Score.java
@@ -0,0 +1,63 @@
+package crack.cduestc.jw.score;
+
+/**
+ * 成绩信息实体类
+ *
+ * @author Ketuer
+ * @since 1.0
+ */
+public class Score {
+    String id;
+    /* 名称 */
+    String name;
+    /* 学分 */
+    double value;
+    /* 成绩 */
+    double score;
+    /* 属性 */
+    String type;
+    /* 原因 */
+    String reason;
+
+    public Score(String id, String name, String value, String score, String type, String reason) {
+        this.id = id;
+        this.name = name;
+        this.value = Double.parseDouble(value);
+        this.score = Double.parseDouble(score);
+        this.type = type;
+        this.reason = reason;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Score{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", value=" + value +
+                ", score=" + score +
+                ", type='" + type + '\'' +
+                ", reason='" + reason + '\'' +
+                '}';
+    }
+}
